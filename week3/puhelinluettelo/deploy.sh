33284fc4e8b7cd531bf3eb68c09ebd9bb2d5670c
Jarkko Karttunen <jarkko.karttunen1@gmail.com>
2020-11-26 12:15:15 +0200

    part0

--- week3/puhelinluettelo/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-npm run build
-rm -rf ../../../fullstack-heroku/build
-cp -r build ../../../fullstack-heroku
--- week3/valtiot/src/app.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import {SearchForm} from './searchForm'
-
-export const App = () => <SearchForm />
